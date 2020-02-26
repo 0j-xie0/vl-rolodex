b7ccf3b20377e575f9a646bce1796992d8908993
J Xie <j.xie@post.harvard.edu>
2020-02-25 22:24:20 -0500

    deploy prep

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext.js';
+import ContactContext from '../../context/contact/contactContext.js';
+import Logo from './logo.svg';
+
+const Navbar = ({ title }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>{user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/about'>about</Link>
+      </li>
+      <li>
+        <Link to='/register'>register</Link>
+      </li>
+      <li>
+        <Link to='/login'>login</Link>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <img src={Logo} alt={''} /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+Navbar.defaultProps = {
+  title: 'Valence Logic Rolodex'
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        Full Stack Application created with React NodeJS Express MongoDBAtlas
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 2.5.3
+      </p>
+    </div>
+  );
+};
+
+export default About;
